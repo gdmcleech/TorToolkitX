b7991825ee86a0fd734223d793c3217d7e8e59f7
gdmcleech <88363021+gdmcleech@users.noreply.github.com>
2021-08-03 11:40:29 +0600

    Update wserver.py

--- tortoolkit/core/wserver.py
+++ tortoolkit/core/wserver.py
@@ -110,7 +110,7 @@ code_page = """
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <title>
-TorToolKitX Torrent Files
+GDMC Leech Torrent Files
 </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -278,7 +278,7 @@ async def set_priority(request):
 @routes.get("/")
 async def homepage(request):
     return web.Response(
-        text='<h1>🤗TorToolkitX webserver is Up and Running successfully🟢</h1>\n<h2>⚡See TorToolkitX on <a href="https://github.com/XcodersHub/TorToolkitX">@GitHub</a></h2>\n<h2>🥲Join XcodersHub <a href="https://t.me/XcodersHub">TG channel</a></h2>',
+        text='<h1>🤗GDMC Leech webserver is Up and Running successfully🟢</h1>\n<h2>✅ Join GDMC Leech <a href="https://t.me/gdmcleech">GDMC Leech</a></h2>\n<h2>♻️Join Gdrive Mirror Cloud <a href="https://t.me/GdriveMirrorCloud">TG channel</a></h2>',
         content_type="text/html",
     )
 
